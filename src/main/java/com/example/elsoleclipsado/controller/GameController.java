65f678666c93687126083618fe5df695600ce645
Juan Toro <juancarlostorocifuentes12@gmail.com>
2024-09-17 16:47:41 -0500

    Se elimino las ejecuciones por consola y se va a implementar el uso de los botones y el resto de cosas graficas para que el juego quede listo

--- src/main/java/com/example/elsoleclipsado/controller/GameController.java
+++ src/main/java/com/example/elsoleclipsado/controller/GameController.java
@@ -1,20 +1,10 @@
 package com.example.elsoleclipsado.controller;
 
-import com.example.elsoleclipsado.views.GameView;
-import com.example.elsoleclipsado.views.StartView;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class GameController {
-
-    @FXML
-    private Button buttonJugar;
 
     public boolean verificarPalabraValida(String palabra) {
         String regex = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$";
